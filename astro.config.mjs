7457def574e18ad7b9afb16f6bd9b387f23711bf
Juanjo Martínez <juanjodevs@gmail.com>
2024-01-19 10:39:29 +0100

    Added vecel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import markdownIntegration from '@astropub/md';
 import tailwind from "@astrojs/tailwind";
 
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,11 +11,11 @@ export default defineConfig({
     remarkPlugins: [],
     rehypePlugins: []
   },
-  output: "server",
-  adapter: cloudflare(),
   vite: {
     define: {
       'process.env.STRAPI_URL': JSON.stringify(process.env.STRAPI_URL)
     }
-  }
+  },
+  output: "server",
+  adapter: vercel()
 });
