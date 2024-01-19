c46177bb3ca468f42bc3dfd8b871688f2dac31d9
Juanjo Martínez <juanjodevs@gmail.com>
2024-01-19 10:32:05 +0100

    Added cloudflare support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import markdownIntegration from '@astropub/md';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +12,10 @@ export default defineConfig({
     rehypePlugins: []
   },
   output: "server",
-  adapter: vercel()
+  adapter: cloudflare(),
+  vite: {
+    define: {
+      'process.env.STRAPI_URL': JSON.stringify(process.env.STRAPI_URL)
+    }
+  }
 });
